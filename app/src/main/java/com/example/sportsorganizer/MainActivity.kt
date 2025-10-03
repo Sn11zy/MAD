419b4c25d85cb1c4846e8c9e9c6db7273eb9c39a
Philip Paskov <philippaskov@Philips-MacBook-Pro.local>
2025-10-03 09:46:31 +0300

    first home screen and main activity change

---
+++ app/src/main/java/com/example/sportsorganizer/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.sportsorganizer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.sportsorganizer.ui.screens.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+
+
